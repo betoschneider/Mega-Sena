5a9d6f4fb290671ba5592a6751efdc267ef106a0
betoschneider <roberto.schneider@gmail.com>
2023-12-15 08:08:37 -0300

    sem números repetidos

--- numeros_da_sorte.py
+++ numeros_da_sorte.py
@@ -2,18 +2,23 @@
 class numero_da_sorte():
     def __init__(self):
         pass
-        
-    def gerar(self, qtd):
+    def gerar(self, qtd_jogos, qtd_dezendas=6):
         import numpy as np
-        for i in range(qtd):
-            a = np.array([np.random.randint(0, 61),
-                  np.random.randint(0, 61),
-                  np.random.randint(0, 61),
-                  np.random.randint(0, 61),
-                  np.random.randint(0, 61),
-                  np.random.randint(0, 61)]
-                  )
-            print(np.sort(a))
+        def numero_magico():
+            return np.random.randint(1, 61)
+        for i in range(qtd_jogos):
+            jogo = [numero_magico()]
+
+            for j in range(qtd_dezendas - 1):
+                  chave = True
+                  while chave:
+                      novo_num = numero_magico()
+                      if novo_num not in jogo:
+                          jogo.append(novo_num)
+                          chave = False
+
+            print(np.sort(jogo))
+            
 
 if __name__ == '__main__':
     n = numero_da_sorte()
